cef1937bf1ad52bfd91c80f4344b3513386a9695
Chaitanya134 <chaitanyagiri2@gmail.com>
2023-10-18 04:48:54 +0530

    Fix Skill Cloud Size

--- components/SkillsCloud.tsx
+++ components/SkillsCloud.tsx
@@ -22,13 +22,11 @@ const skills: string[] = ['JavaScript', 'React.js', 'MongoDb', 'MySQL', 'PHP', '
 
 const SkillsCloud: React.FC = () => {
     return (
-        <div style={{ fontFamily: "Roboto" }}>
-            <Canvas dpr={[1, 2]} camera={{ position: [0, 0, 35], fov: 90 }}>
-                <fog attach="fog" args={['#202025', 0, 80]} />
-                <Cloud count={4} radius={20} />
-                <OrbitControls autoRotate enableZoom={false} panSpeed={0.3} />
-            </Canvas>
-        </div>
+        <Canvas dpr={[1, 2]} camera={{ position: [0, 0, 35], fov: 90 }}>
+            <fog attach="fog" args={['#202025', 0, 80]} />
+            <Cloud count={4} radius={20} />
+            <OrbitControls autoRotate enableZoom={false} panSpeed={0.3} />
+        </Canvas>
     )
 }
 
